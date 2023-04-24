1b4989b2025ed91990cddc914d5d9fa07941f065
norki <norkink89@mail.ru>
2023-04-25 00:04:29 +0400

    completed

---
+++ src/main/java/Application.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+public class Application {
+    public static void main(String[] args) throws SQLException {
+
+        final String user = "postgres";
+        final String password = "Dom88173827027";
+        final String url = "jdbc:postgresql://localhost:5432/employee";
+
+        try (final Connection connection = DriverManager.getConnection(user, password, url);
+             PreparedStatement statement = connection.prepareStatement("" + "SELECT * FROM employeeList WHERE id = (?)")) {
+            statement.setInt(1, 6);
+            final ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String name = "Имя: " + resultSet.getString("first_name");
+                String surname = "Фамилия: " + resultSet.getString("last_name");
+                String gender = "Пол: " + resultSet.getString("gender");
+                int age = resultSet.getInt("age");
+
+                System.out.println(name);
+                System.out.println(surname);
+                System.out.println(gender);
+                System.out.println("Возраст: " + age);
+            }
+        }
+
+        EmployeeDao employeeDao = new EmployeeDaoImpl();
+        System.out.println(employeeDao.getAllEmployee());
+        Employee employee = new Employee(1, "Мария", "Иванова", "fem", 37, new City(5, "Тобольск"));
+        employeeDao.add(employee);
+        System.out.println(employeeDao.getAllEmployee());
+
+        employee.setLast_name("Жукова");
+        employeeDao.updateEmployee(10, employee);
+        System.out.println(employeeDao.getById(10));
+        employeeDao.deleteEmployee(4);
+        System.out.println(employeeDao.getAllEmployee());
+    }
+
+}
---
+++ src/main/java/EmployeeDao.java
@@ -0,0 +1,8 @@
+import java.util.List;
+public interface EmployeeDao {
+void add (Employee employee);
+Employee getById (int id);
+List <Employee> getAllEmployee ();
+void updateEmployee (int id, Employee employee);
+void deleteEmployee (int id);
+}
